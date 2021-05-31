877790cad410c30b50bd86baed6766b4ad63a8a9
wufan <wxiao2@uw.edu>
2021-05-31 04:44:20 +0000

    re-edit

--- mem_impl.h
+++ mem_impl.h
@@ -11,24 +11,18 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-typedef struct freeNode {
+typedef struct freeListNode {
     uintptr_t size;
     uintptr_t addr;
-    struct freeNode* next;
-    struct freeNode* front;
-}freeNode;
+    struct freeListNode* next;
+    struct freeListNode* front;
+}freeListNode;
 
-extern freeNode* freeList;
-<<<<<<< HEAD
+extern freeListNode* freeList;
 extern uintptr_t total_size;
 extern uintptr_t total_free;
 extern uintptr_t n_free_blocks;
-void check_heap();
-freeListNode* split(freeListNode* curr_block, uintptr_t size);
 freeListNode* split(freeListNode* curr_block, uintptr_t size);
 void insert(freeListNode* insertion_block);
-
-=======
 void check_heap();
->>>>>>> 7b3ea6ba7ea6b48c9a90325a7c47545f856d0842
 #endif
